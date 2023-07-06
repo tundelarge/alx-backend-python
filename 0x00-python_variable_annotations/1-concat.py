d065ad4d74486616e1782c988b4dfb6c9c9a19cf
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-06 17:46:15 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,7 @@
-qwertyu
+#!/usr/bin/env python3
+"""Contains a single function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings."""
+    return str1 + str2
