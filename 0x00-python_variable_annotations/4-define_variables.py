4cbbdd2513e1a3c5e9e8406d6b76060ce5adbffc
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-06 17:48:15 +0100

    Update 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,1 +1,7 @@
-qwertyu
+#!/usr/bin/env python3
+"""Contains definition of variables with specified values and annotations."""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
